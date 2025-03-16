8edfd56d5d39fb44f98cbcbcdbdd37e383f64501
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-03-16 23:01:03 +0000

    Testing sigmoid

---
+++ Week3/Lab03.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_common import plot_data, sigmoid, draw_vthresh
+plt.style.use('./deeplearning.mplstyle')
+
+X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1)
+
+# Plot sigmoid(z) over a range of values from -10 to 10
+z = np.arange(-10,11)
+
+fig,ax = plt.subplots(1,1,figsize=(5,3))
+# Plot z vs sigmoid(z)
+ax.plot(z, sigmoid(z), c="b")
+
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+draw_vthresh(ax,0)
