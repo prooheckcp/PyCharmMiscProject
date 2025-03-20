c6f073a6ed392bbcb621457a9841a274d4f3c5b0
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-03-20 23:58:33 +0000

    compute_gradient_linear_reg

--- Week3/Lab09.py
+++ Week3/Lab09.py
@@ -41,14 +41,36 @@ def compute_cost_logistic_reg(X, y, w, b, lambda_=1):
 
     return cost + cost_reg
 
+def compute_gradient_linear_reg(X, y, w, b, lambda_=1):
+    m, n = X.shape
+
+    dj_db = 0
+    dj_dw = np.zeros(n)
+
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b
+        err = f_wb_i - y[i]
+
+        dj_db += err
+
+        for j in range(n):
+            dj_dw[j] += err * X[i, j]
+
+    dj_db /= m
+    dj_dw /= m
+
+    for j in range(n):
+        dj_dw[j] += (lambda_/m) * w[j]
+
+    return dj_db, dj_dw
+
 np.random.seed(1)
-X_tmp = np.random.rand(5,6)
+X_tmp = np.random.rand(5,3)
 y_tmp = np.array([0,1,0,1,0])
-w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+w_tmp = np.random.rand(X_tmp.shape[1])
 b_tmp = 0.5
 lambda_tmp = 0.7
-cost_tmp = compute_cost_logistic_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+dj_db_tmp, dj_dw_tmp =  compute_gradient_linear_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
 
-print("Regularized cost:", cost_tmp)
-
-
+print(f"dj_db: {dj_db_tmp}", )
+print(f"Regularized dj_dw:\n {dj_dw_tmp.tolist()}", )
