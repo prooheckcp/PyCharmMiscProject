dbdb1e4c6c87606346845314d48495725aad4a56
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-03-20 23:42:21 +0000

    compute_cost_logistic_reg

--- Week3/Lab09.py
+++ Week3/Lab09.py
@@ -4,8 +4,6 @@ from plt_overfit import overfit_example, output
 from lab_utils_common import sigmoid
 np.set_printoptions(precision=8)
 
-x_train = np.array([[1, 2, 3]])
-
 def compute_cost_linear_reg(X, y, w, b, lambda_=1):
     m, n = X.shape
 
@@ -25,3 +23,32 @@ def compute_cost_linear_reg(X, y, w, b, lambda_=1):
     total_cost = cost + reg_cost
 
     return total_cost
+
+def compute_cost_logistic_reg(X, y, w, b, lambda_=1):
+    m, n = X.shape
+
+    cost = 0
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i], w) + b)
+        cost += -y[i] * np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i)
+
+    cost/= m
+
+    cost_reg = 0
+    for j in range(n):
+        cost_reg += w[j] ** 2
+    cost_reg *= (lambda_/(2*m))
+
+    return cost + cost_reg
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_logistic_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print("Regularized cost:", cost_tmp)
+
+
